00e179fa95c0162cecd346483314e8dc722a9f21
MadhanKumar22it27 <mathanbhuvana24@gmail.com>
2025-07-29 23:33:46 +0530

    updated4 commit

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 // app/page.js
-import { personalData } from "@/utils/data/personal-data";
+import { personalData } from "../../utils/data/personal-data";
 import AboutSection from "../../components/homepage/about";
 import Blog from "../../components/homepage/blog";
-import ContactSection from "@/components/homepage/contact";
-import Education from "@/components/homepage/education";
-import Experience from "@/components/homepage/experience";
-import HeroSection from "@/components/homepage/hero-section";
-import Projects from "@/components/homepage/projects";
-import Skills from "@/components/homepage/skills";
+import ContactSection from "../../components/homepage/contact";
+import Education from "../../components/homepage/education";
+import Experience from "../../components/homepage/experience";
+import HeroSection from "../../components/homepage/hero-section";
+import Projects from "../../components/homepage/projects";
+import Skills from "../../components/homepage/skills";
 
 export const dynamic = "force-dynamic"; // Optional
 
